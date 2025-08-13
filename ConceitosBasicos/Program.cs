cc61cb966565eb490575db87a05a8bae1f86711a
offmicael <zmicaelvasco@gmail.com>
2025-08-12 21:05:57 -0300

    IRPF

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,7 +1,18 @@
-﻿Console.WriteLine("Informe seu nome: ");
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Informe seu nome: ");
 string nome = Console.ReadLine();
 Console.WriteLine("Informe seu salário: ");
 decimal salario = decimal.Parse(Console.ReadLine());
 
 Console.WriteLine($"Seu nome é {nome} e seu salário é {salario}");
 
+//informe o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto IRPF
+
+if (salario <= 2259.20);
+{
+    Console.WriteLine("Isento da taxa IRPF");
+}
+else if (salario >= 2259.20) && salario <=2826.65 )
+}
+        decimal 
