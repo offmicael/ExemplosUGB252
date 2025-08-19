df199111270a68650d88774fc439d9cfa4cd008e
offmicael <zmicaelvasco@gmail.com>
2025-08-19 20:10:10 -0300

    Cálculo de IRPF

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,24 +1,36 @@
-﻿using System.ComponentModel.Design;
+﻿try
+{
+    Console.WriteLine("Informe seu nome: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Informe seu salário: ");
+    decimal salario = decimal.Parse(Console.ReadLine());
 
-Console.WriteLine("Informe seu nome: ");
-string nome = Console.ReadLine();
-Console.WriteLine("Informe seu salário: ");
-decimal salario = decimal.Parse(Console.ReadLine());
+    Console.WriteLine($"Seu nome é {nome} e seu salário é {salario}");
 
-Console.WriteLine($"Seu nome é {nome} e seu salário é {salario}");
+    //informe o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto IRPF
 
-//informe o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto IRPF
+    decimal irpfDevido;
+    if (salario <= 2259.20m)
+        irpfDevido = 0;
+    else if (salario <= 2826.65m)
+        irpfDevido = salario * 0.075m - 169.21m;
+    else if (salario <= 3751.05m)
+        irpfDevido = salario * 0.015m - 381.44m;
+    else if (salario <= 4664.68m)
+        irpfDevido = salario * 0.0225m - 662.77m;
+    else
+        irpfDevido = salario * 0.0275m - 896m;
 
-decimal irpfDevido;
-if (salario <= 2259.20m)
-    irpfDevido = 0;
-else if (salario <= 2826.65m)
-    irpfDevido = salario * 0.075m - 169.21m;
-else if (salario <= 3751.05m)
-    irpfDevido = salario * 0.015m - 381.44m;
-else if (salario <= 4664.68m)
-    irpfDevido = salario * 0.0225m - 662.77m;
-else
-    irpfDevido = salario * 0.0275m - 896m;
+    Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é {irpfDevido}, então o salário com desconto de IRPF será {salario - irpfDevido}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Osalario de ser informado em formato númerico.");
+}
+catch (Exception e)
+{
+    Console.WriteLine("Ocorreu um erro. informe ao suporte técnico");
 
-Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é {irpfDevido}, então o salário com desconto de IRPF será {salario - irpfDevido}");
+    Console.WriteLine(e.StackTrace);
+}
+
